e9608f33909c3151c7ab78a933f178a435819799
Jure Sulič <j.sulic@gmail.com>
2026-06-20 02:09:23 +0300

    updated objects

--- webpage/src/components/world.js
+++ webpage/src/components/world.js
@@ -48,11 +48,13 @@ function loadWorld(scene, worldOctree) {
     }
   });
 
-  loadModel(loader, "collision-world-01.glb", scene, worldOctree);
+  loadModel(loader, "collision-world-01.gltf", scene, worldOctree);
   loadModel(loader, "collision-world-02.gltf", scene, worldOctree);
-  loadModel(loader, "collision-world-03.glb", scene, worldOctree);
-  loadModel(loader, "collision-world-04.glb", scene, worldOctree);
-  loadModel(loader, "collision-world-05.glb", scene, worldOctree);
+  loadModel(loader, "collision-world-03.gltf", scene, worldOctree);
+  loadModel(loader, "collision-world-04.gltf", scene, worldOctree);
+  loadModel(loader, "collision-world-05.gltf", scene, worldOctree);
+
+  loadModel(loader, "bottom.gltf", scene, worldOctree);
 }
 
 export { loadWorld, worldAnimationMixers };

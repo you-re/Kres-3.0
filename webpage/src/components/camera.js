227610f0f56772c852db8bcefb6747a623e867cd
Jure Sulič <j.sulic@gmail.com>
2026-06-23 22:42:54 +0300

    changed camera fov

--- webpage/src/components/camera.js
+++ webpage/src/components/camera.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 function createCamera(scene) {
   const camera = new PerspectiveCamera(
-    70,
+    30,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
